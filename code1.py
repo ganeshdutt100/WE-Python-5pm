d26771a7586c0f0952a149066d088ee2591b4783
Ganesh Dutt <gdutt2304@gmail.com>
2026-09-12 17:55:43 +0530

    first commit

---
+++ code1.py
@@ -0,0 +1,23 @@
+# print("Hello")
+# 
+# a = 45
+# b = 78
+# c   =  a+b 
+# print(c)
+
+# a  = 67
+# b  = 3
+# output  =  a + b
+# output  =  a - b
+# output  =  a * b
+# output  =  a / b
+# output  =  a // b
+# output  =  a ** b
+
+
+
+
+
+print(output) 
+
+
