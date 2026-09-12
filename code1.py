4f3fa5554e3399e3d9de1b394f4f9b57033c2434
Ganesh Dutt <gdutt2304@gmail.com>
2026-09-12 18:08:08 +0530

    Operators

--- code1.py
+++ code1.py
@@ -7,17 +7,50 @@
 
 # a  = 67
 # b  = 3
+
+
 # output  =  a + b
 # output  =  a - b
 # output  =  a * b
 # output  =  a / b
 # output  =  a // b
 # output  =  a ** b
+# output   = a % b 
 
 
 
 
+# a  = 7
+# b  = 9
+# output  = a > b
+# output  = a == b
+# output  = a != b
+# output  = a >= b
+# output  = a <= b
 
-print(output) 
+# and , or , not 
+#  
+# True  and  True = True
+# False  and  True = False
+# False  and  False = False
+# 
+# 
+#  
+# True  or   True = True
+# False  or   True = True  
+# False  or   False = False
+
+# output  =  (not(True))
+
+# a = 6
+# a= a+5
+# a +=5
+# a -=5
+# a *=5
+# a /=5
+
+
+
+print(a) 
 
 
