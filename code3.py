21c786d453898cc26b2a52f37a983f705c5f4f56
Ganesh Dutt <gdutt2304@gmail.com>
2026-09-19 17:58:37 +0530

    Questions

---
+++ code3.py
@@ -0,0 +1,62 @@
+# if condition :
+#    stm    
+
+# age  = 18 
+# if age >= 18:
+#   print("you are eligible to vote ")
+
+
+# value  =  44
+# 
+# if value % 2  == 0:
+#     print("Even Number ")
+# else :
+#     print("Odd Number ")    
+
+
+# marks   = 90
+# 
+# if marks >= 90:
+#     print("Grade :  A")
+# elif marks >= 75:
+#     print("Grade :  B") 
+# elif marks >= 50:
+#     print("Grade : C")
+# else:
+#     print("Grade :   F")           
+
+# num  =  0
+
+# age  =  45
+# if age < 12:
+#     ticket_price = 8
+# elif age >= 65:
+#    ticket_price = 10
+# else:
+#    ticket_price = 15
+# print("Ticket Price :  ", ticket_price)
+
+# user_role = "Editor"
+# is_logged_in =  False
+# 
+# if not is_logged_in:
+#     print("please login first")
+# elif user_role == "Admin":
+#     print("Full Access ")
+# elif user_role == "Editor":
+#     print("Edit Access")
+# elif user_role == "viewrs":
+#     print("Read-Only") 
+# else:
+#      print("Access Denied")               
+
+
+num  = 15
+if  num % 3 == 0 and num % 5 == 0:
+    print("FizzBuzz")
+elif num %3 == 0:
+    print("Fizz")
+elif num % 5 == 0:
+    print("Buzz")
+else:
+    print(num)            
