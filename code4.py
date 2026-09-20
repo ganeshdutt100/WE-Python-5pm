7dcf725393dce466a806c029e423b70c3408a61d
Ganesh Dutt <gdutt2304@gmail.com>
2026-09-20 18:40:37 +0530

    Game

--- code4.py
+++ code4.py
@@ -40,15 +40,15 @@
 #     print("Please login first ")    
 
 
-year  = 1600
-
-if  year  % 4  ==  0:
-    if year % 100  == 0:
-        if year % 400 == 0:
-           print(year , "is a Leap year ")
-        else :
-            print( year  ,  "is not a leap year ")    
-    else:
-        print(year , "is a leap year ")
-else:
-    print( year  ,  "is not a leap year ")    
+# year  = 1600
+# 
+# if  year  % 4  ==  0:
+#     if year % 100  == 0:
+#         if year % 400 == 0:
+#            print(year , "is a Leap year ")
+#         else :
+#             print( year  ,  "is not a leap year ")    
+#     else:
+#         print(year , "is a leap year ")
+# else:
+#     print( year  ,  "is not a leap year ")    
