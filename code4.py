98324bc3bcb9161598d6add963901fc5121f43a2
Ganesh Dutt <gdutt2304@gmail.com>
2026-09-20 17:36:31 +0530

    nested Questions

---
+++ code4.py
@@ -0,0 +1,40 @@
+# num   =  0
+# if num >=  0:
+#     print("the number is positive ")    
+#     if num == 0:
+#              print("the number is zero ")     
+# 
+# else:
+#     print("the number is negative ")    
+# 
+# Questions 1 :   School Attendance & Exam
+# Scenario: A student wants to give their final exam. First, check if their attendance is 75% or higher. If it is, then check if they have paid their exam fees.
+# 
+# Variables to create: attendance_percentage (number) and fees_paid (boolean True/False).
+# 
+# Task: Write a nested if-else to print if they can sit for the exam.
+
+# attendance  = 75
+# fees_paid  = False
+# 
+# if attendance >= 75:
+#     print("attendance is good")
+#     if fees_paid == True:
+#         print("You can sit")
+#     else:
+#         print("you cannot sit ")
+# 
+# else:
+#     print("attendance is too low")    
+
+# is_logged_in = True
+# cart_items = 5
+# 
+# if is_logged_in == True:
+#     print("Welcome Back ")
+#     if cart_items > 0:
+#       print()
+#     else:
+#        print()
+# else:
+#     print()    
