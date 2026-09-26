20927c9364a4e3311813ffcc8966048af3b00fa6
Ganesh Dutt <gdutt2304@gmail.com>
2026-09-26 17:33:34 +0530

    Loop

---
+++ code5.py
@@ -0,0 +1,33 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+# print(6)
+# 1 , 100
+# start   =1 
+# end  = 101 (n-1)
+
+# for(range(5))
+# for i in range(5):
+#     print(i)
+
+# for i in range(5,21):
+#     print(i)
+
+# for i in range(2 , 21 , 2):
+#     print(i)
+
+
+# i  =  10 
+# while i <= 20:
+#     print(i)
+
+# i  =  10 
+# while i <= 20:
+#     print(i)
+#     i =  i+1
+
+# for i in range(5):
+#     print(i, end=" ")
+
