a35da00e5e396e9c6d1132085817778fc95687a8
Ganesh Dutt <gdutt2304@gmail.com>
2026-09-26 18:23:44 +0530

    loop Questions

--- code5.py
+++ code5.py
@@ -31,3 +31,56 @@
 # for i in range(5):
 #     print(i, end=" ")
 
+# name  =  "PYTHON"
+# for i in name:
+#     print(i)
+
+# 0 to 20  sum 
+
+# 
+# sum  = 0 
+# 
+# for i in range(0,21):
+#     # print(sum , i )
+#     sum  = sum +i
+# print(sum)
+
+
+#     sum  = 10
+#     i  = 5
+# 
+#     sum  = 15
+# EDUCATION
+
+# 5  =  5 *4*3*2*1 =  120 
+# 5  =  1*2*3*4*5 =  120 
+# 
+# fact  =  1
+# number  =  5
+# 
+# for i in range(1,number+1):
+#     fact  = fact*i
+# print(fact)
+
+
+# word  =  "EDUCATION"
+# vowel_count  =  0
+# for letter in word:
+#     if letter in "AEIOUaeiou":
+#         vowel_count = vowel_count + 1
+# print(vowel_count)
+
+# print()
+
+while True:
+    p =  input("Do you want to run the program : yes / no ")
+    if p == "yes":
+     print("Program is running...")
+     print("task completed ")
+
+    elif p  ==  "no":
+       print("Program is shutdown ")
+       break
+    else:
+       print("invalid  ")
+print("program closed")       
